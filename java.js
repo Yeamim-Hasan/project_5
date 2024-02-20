ad6ce1833c1a1b74a1fd676fab2403f0037404e2
Yeamim-Hasan <yeamimhasan98@gmail.com>
2024-02-20 23:11:51 +0600

    first push

---
+++ java.js
@@ -0,0 +1,194 @@
+    function press1(){
+        const A1 = document.getElementById('A1');
+        // console.log('ststs');
+        // A1.classList.add('w-[75px]')
+        const a1 = document.getElementById('a1');
+        a1.classList.remove('hidden');
+        
+        const number = document.getElementById('number');
+        // console.log('jhd');
+       const inr = number.innerText;
+       console.log(inr);
+       const inrint = parseInt(inr);
+       console.log(inrint);
+        const num = inrint - 1 ;
+        console.log(num);
+        number.innerText =num;
+     A1.disabled = true;
+        
+        // const total = (inrint-num) * 550;
+        // console.log(total);
+        const finalTotal = document.getElementById('ttl');
+        const sub = parseInt( ttl.innerText);
+        console.groupCollapsed(sub);
+        const inalTotal = sub + 550;
+        console.log(inalTotal);
+        ttl.innerText = inalTotal;
+        const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    // function press1(){
+    //     const A1 = document.getElementById('A1');
+    //     // console.log('ststs');
+    //     // A1.classList.add('w-[75px]')
+    //     const a1 = document.getElementById('a1');
+    //     a1.classList.remove('hidden');
+        
+    //     const number = document.getElementById('number');
+    //     // console.log('jhd');
+    //    const inr = number.innerText;
+    //    console.log(inr);
+    //    const inrint = parseInt(inr);
+    //    console.log(inrint);
+    //     const num = inrint - 1 ;
+    //     console.log(num);
+    //     number.innerText =num;
+    //  A1.disabled = true;
+    //     const total = (inrint-num) * 550;
+    //     console.log(total);
+    //     const finalTotal = document.getElementById('ttl');
+    //     ttl.innerText = total;
+    //     console.log(finalTotal);
+    // }
+    function press2(){
+        const A2 = document.getElementById('A2');
+        const a2 = document.getElementById('a2');
+        a2.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+       console.log(inrint);
+        const num = inrint - 1 ;
+        console.log(num);
+        number.innerText =num;
+        console.log(number);
+        A2.disabled = true;
+        const total = (inrint-num);
+        console.log(inrint);
+        
+        console.log(total);
+          // console.log(total);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          console.log(inalTotal);
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    function press3(){
+        const A3 = document.getElementById('A3');
+        const a3 = document.getElementById('a3');
+        a3.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+        const num = inrint - 1 ;
+        number.innerText =num;
+        A3.disabled = true;
+        const total = (inrint-num);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    function press4(){
+        const A4 = document.getElementById('A4');
+        const a4 = document.getElementById('a4');
+        a4.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+        const num = inrint - 1 ;
+        number.innerText =num;
+        A4.disabled = true;
+        const total = (inrint-num);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    function press5(){
+        const B1 = document.getElementById('B1');
+        const b1 = document.getElementById('b1');
+        b1.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+        const num = inrint - 1 ;
+        number.innerText =num;
+        B1.disabled = true;
+        const total = (inrint-num);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    function press6(){
+        const B2 = document.getElementById('B2');
+        const b2 = document.getElementById('b2');
+        b2.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+        const num = inrint - 1 ;
+        number.innerText =num;
+        B2.disabled = true;
+        const total = (inrint-num);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    function press7(){
+        const B3 = document.getElementById('B3');
+        const b3 = document.getElementById('b3');
+        b3.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+        const num = inrint - 1 ;
+        number.innerText =num;
+        B3.disabled = true;
+        const total = (inrint-num);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    function press8(){
+        const B4 = document.getElementById('B4');
+        const b4 = document.getElementById('b4');
+        b4.classList.remove('hidden');
+        const number = document.getElementById('number');
+       const inr = number.innerText;
+       const inrint = parseInt(inr);
+        const num = inrint - 1 ;
+        number.innerText =num;
+        B4.disabled = true;
+        const total = (inrint-num);
+          const finalTotal = document.getElementById('ttl');
+          const sub = parseInt( ttl.innerText);
+          console.groupCollapsed(sub);
+          const inalTotal = sub + 550;
+          ttl.innerText = inalTotal;
+          const grand = document.getElementById('grandTota')
+        grandTota.innerText =inalTotal;
+    }
+    
